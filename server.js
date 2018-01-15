d990ef53a2c55b78ddbc2c7aabb07f0865960a6f
DeGrandis <robd@vt.edu>
2018-01-15 01:31:02 -0500

    added nicer comments to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//required imports
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -8,38 +9,44 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var mqtt = require('mqtt');
 var favicon = require('serve-favicon');
-
-//comment
-var client  = mqtt.connect('mqtt:192.168.1.242:1883');
-var url = 'mongodb://rob:rob@ds251277.mlab.com:51277/homeautomationdeg';
-
 var ejs = require('ejs');
 
+//connect to mqtt server, will be a local IP
+var client  = mqtt.connect('mqtt:192.168.1.242:1883');
+//url to public mongodb database
+var url = 'mongodb://rob:rob@ds251277.mlab.com:51277/homeautomationdeg';
+
+//setting app specific defaults
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname + '/views/styles'));
-
 app.use(favicon(path.join(__dirname, 'views','assets','favicon.ico')));
 
+//test connect to Mongodb
 MongoClient.connect(url, function(err, db) {
   console.log("Database: Connected");
   db.close();
 });
 
+//test connect to mqtt
 client.on('connect', function () {
   console.log('MQTT: Connected');
 })
 
+//------------------------
+//ROUTES
+//------------------------
 
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
+//GET /settings
+//connects to mongodb and fetches trigger data.  Then renders a page
+//with query results.
 app.get('/settings', function(req, res) {
-
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         db.collection("Days").find().toArray(function(err, result) {
@@ -74,14 +81,14 @@ app.get('/settings', function(req, res) {
             res.render('pages/settings', {result: result});
         });
     });
-
-    //res.render('pages/settings');
 });
 
+//POST /settings
+//inserts a form entry into the database, then redirects to /settings
+//to avoid a double POST request on accidental refresh.
 app.post('/settings', function(req, res) {
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-
         db.collection("Days").insertOne(req.body, function(err, ress) {
             if (err) throw err;
             console.log("1 document inserted");
@@ -89,15 +96,14 @@ app.post('/settings', function(req, res) {
             res.redirect('/settings');
         });
     });
-
 });
 
+//GET /deleted
+//deletes an entry from the database
 app.get('/delete/:id', function(req, res) {
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var myquery = { "_id": ObjectId(req.params.id) };
-      //var myquery1 = { "day": "0" };
-
       db.collection("Days").deleteOne(myquery, function(err, obj) {
         if (err) throw err;
         console.log("1 document deleted");
@@ -105,17 +111,24 @@ app.get('/delete/:id', function(req, res) {
         res.redirect('/settings');
       });
     });
-
 });
 
+//logs socket connection
 io.on('connection', function(socket) {
     console.log("Connection.");
 })
 
+//starts server
 http.listen(3000, function() {
     console.log('Server Running.');
 });
 
+//uses built in javascript data functions and stores it to a variable.
+//polls the database and checks to see if any entry matches the current time.
+//if anything matches the current time then it publishes a status message
+//to the stored mqtt topic.  
+//**does this every second.
+//*****THERE IS PROBABLY A MORE ELEGANT WAY TO DO THIS.
 setInterval(function() {
     var current = new Date();
     var day = current.getDay();
@@ -136,6 +149,5 @@ setInterval(function() {
             db.close();
         });
     });
-
     console.log("Day: " + day + " Time: " + time);
 }, 1000);
